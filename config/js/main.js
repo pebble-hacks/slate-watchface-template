1e21a71687e54f2658e2c3fee1bfed9f15174019
Kirby <kirby@pebble.com>
2015-07-30 15:30:54 -0400

    Add webviewclosed event listener.

--- config/js/main.js
+++
@@ -1,8 +0,0 @@
-var $submitButton = $('#submitButton');
-
-$submitButton.on('click', function() {
-  console.log('Submit');
-
-  var return_to = getQueryParam('return_to', 'pebblejs://close#');
-  document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
-});
