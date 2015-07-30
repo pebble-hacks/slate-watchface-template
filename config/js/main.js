31d7d36eb387315e6c8c714733d26b289b714b5b
Kirby <kirby@pebble.com>
2015-07-30 15:30:23 -0400

    Add submit logic.

--- config/js/main.js
+++ config/js/main.js
@@ -6,15 +6,3 @@ $submitButton.on('click', function() {
   var return_to = getQueryParam('return_to', 'pebblejs://close#');
   document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
 });
-
-function getQueryParam(variable, defaultValue) {
-  var query = location.search.substring(1);
-  var vars = query.split('&');
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split('=');
-    if (pair[0] === variable) {
-      return decodeURIComponent(pair[1]);
-    }
-  }
-  return defaultValue || false;
-}
