b58e016b8e80a9d409a2c22b878ba27a207e5296
Kirby <kirby@pebble.com>
2015-07-30 15:31:27 -0400

    Add ready event listener.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,11 +1,3 @@
 Pebble.addEventListener('ready', function() {
   console.log('PebbleKit JS ready!');
 });
-
-Pebble.addEventListener('showConfiguration', function() {
-  var url = 'https://kirby.ngrok.io';
-
-  console.log('Showing configuration page: ' + url);
-
-  Pebble.openURL(url);
-});
